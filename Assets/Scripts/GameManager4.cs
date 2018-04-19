607e24f964d88f8fa2a9b8e23b0e9071f19400c4
Usuario Local <Usuario Local>
2018-04-19 19:06:42 +0200

    Pues no sé como vamos a entender esta verga

---
+++ Assets/Scripts/GameManager4.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Jackyjjc.Bayesianet;
+
+public class GameManager4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BayesianNode node1 = new BayesianNode("node1", new string[] { "value1", "value2" }, new double[] { 0.4, 0.6 });
+        BayesianNode node2 = new BayesianNode("node2", new string[] { "v1", "v2","v3" }, new double[] { 0.1,0.2,0.7,0.2, 0.6,0.2 },node1);
+        BayesianNetwork network = new BayesianNetwork(node1, node2);
+        VariableElimination ve = new VariableElimination(network);
+
+        double[] probability = ve.Infer("node1", "node2=v1");
+        Debug.Log("Probability of value1: " + probability[0]);
+        Debug.Log("Probability of value2: " + probability[1]);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
